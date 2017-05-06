f5be79265a484e1739e15d4359662b280d83a20d
Stefan Scherer <scherer_stefan@icloud.com>
2017-05-06 09:18:26 +0200

    Update Docker 17.05.0-ce

--- scripts/install-docker.ps1
+++ scripts/install-docker.ps1
@@ -1,13 +1,21 @@
 # https://msdn.microsoft.com/en-us/virtualization/windowscontainers/quick_start/quick_start_windows_10
+$ErrorActionPreference = 'Stop'
+$ProgressPreference = 'SilentlyContinue'
 Set-ExecutionPolicy Bypass -scope Process
 
+$version = "17.05.0-ce"
+
 New-Item -Type Directory -Path "$($env:ProgramFiles)\docker"
+
+Write-Host "Downloading docker-$version.zip"
 $wc = New-Object net.webclient
-$wc.Downloadfile("https://get.docker.com/builds/Windows/x86_64/docker-17.03.1-ce.zip", "$env:TEMP\docker-17.03.1-ce.zip")
-Expand-Archive -Path $env:TEMP\docker-17.03.1-ce.zip -DestinationPath $env:ProgramFiles -Force
-Remove-Item $env:TEMP\docker-17.03.1-ce.zip
+$wc.DownloadFile("https://get.docker.com/builds/Windows/x86_64/docker-$version.zip", "$env:TEMP\docker-$version.zip")
+Write-Host "Extracting docker-$version.zip"
+Expand-Archive -Path "$env:TEMP\docker-$version.zip" -DestinationPath $env:ProgramFiles -Force
+Remove-Item "$env:TEMP\docker-$version.zip"
+
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";$($env:ProgramFiles)\docker", [EnvironmentVariableTarget]::Machine)
 $env:Path = $env:Path + ";$($env:ProgramFiles)\docker"
-. dockerd --register-service -H npipe:// -H 0.0.0.0:2375 -G docker --label os=windows
+. dockerd --register-service -H npipe:// -H 0.0.0.0:2375 -G docker
 
 Start-Service docker
